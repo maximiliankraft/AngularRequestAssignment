d0f9e98d7e52efeec3f22a718b7cb4b1fc964c0f
Maximilian Kraft <kraft@spengergasse.at>
2022-09-27 16:40:32 +0200

    added ip-fetching capabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularRequestAssignment';
+export class AppComponent implements OnInit{
+  title = 'EmptyProject';
+  ipAddress: string = "";
+
+
+  constructor(private http: HttpClient) {}
+
+  fetchIpText(){
+    this.http.get("http://ifconfig.me",{responseType: "text"}).subscribe(response => {
+      console.log(response);
+      this.ipAddress = response;
+    });
+  }
+
+  fetchIpJson(){
+    this.http.get("http://ifconfig.me/all.json",{responseType: "json"}).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  ngOnInit(): void {
+    this.fetchIpText();
+    this.fetchIpJson();
+
+  }
+
+  // todo add http requests
+  // https://angular.io/guide/http
+
 }
+
